b77911f8d0a2cb89bea5c108b059ffcc4b79fe2e
Michael Johnson <michael.johnson7c5@gmail.com>
2023-07-25 17:14:13 -0400

    created Graph class and unit test

--- src/Graph.js
+++ src/Graph.js
@@ -1,5 +1,81 @@
 class Graph {
+  constructor () {
+    this._nodes = []
+  }
 
+  get nodes () { return this._nodes.slice(0) }
+
+  get values () { return this._nodes.map(node => node.value) }
+
+  get size () { return this._nodes.length }
+
+  includes (value) {
+    return !this._nodes.every(node => node.value !== value)
+  }
+
+  find (value) {
+    return this._nodes.find(node => node.value === value)
+  }
+
+  insert (value) {
+    let node
+    if (this.includes(value)) {
+      node = this.find(value)
+    } else {
+      node = new GraphNode(value)
+      this._nodes.push(node)
+    }
+    return node
+  }
+
+  link (fromValue, toValue, cost) {
+    let fromNode = this.find(fromValue)
+    let toNode = this.find(toValue)
+    if (!(fromNode && toNode)) {
+      throw new Error('Attempted to link unincluded values')
+    } else {
+      fromNode.link(toNode, cost)
+    }
+  }
+
+  linkAll (cost, overwrite) {
+    this._nodes.forEach((fromNode)=>{
+      this._nodes.forEach((toNode)=>{
+        if (fromNode !== toNode && (overwrite || !fromNode.hasLink(toNode))) {
+          fromNode.link(toNode, cost)
+        }
+      })
+    })
+  }
+}
+
+class GraphNode {
+  constructor (value) {
+    this._value = value
+    this._links = []
+  }
+
+  link (node, cost) {
+    if (node === this) {
+      throw new Error('Cannot link a node to itself')
+    }
+    let link = this._links.find(link => link.node === node)
+    if (link) {
+      link.cost = cost
+    } else {
+      this._links.push({node, cost})
+    }
+  }
+
+  hasLink(node) {
+    return this._links.find(link => link.node === node) ? true : false
+  }
+
+  get links () {
+    return this._links.map(link => Object.create(link))
+  }
+
+  get value () { return this._value }
 }
 
 export default Graph
--- src/Graph.test.js
+++ src/Graph.test.js
@@ -1,7 +1,128 @@
 import Graph from './Graph.js'
 
 describe('Graph', ()=>{
-  it('Should exist and be a function', ()=>{
-    expect(typeof Graph).toBe('function')
+  var graph
+
+  beforeEach(()=>{
+    graph = new Graph()
   })
+
+  it('Should exist and be a class', ()=>{
+    expect(typeof Graph).toBe('function')
+    expect(typeof graph).toBe('object')
+  })
+
+  it('Should push new nodes to the node list', ()=>{
+    graph.insert(1)
+    expect(graph.nodes[0].value).toBe(1)
+  })
+
+  it('Should report size as number of nodes in the graph', ()=>{
+    expect(graph.size).toBe(0)
+    graph.insert(1)
+    graph.insert(2)
+    expect(graph.size).toBe(2)
+  })
+
+  it('Should not accept duplicate values', ()=> {
+    graph.insert(1)
+    graph.insert(1)
+    expect(graph.size).toBe(1)
+  })
+
+  it('Should not allow external mutation of node list', ()=> {
+    graph.insert(1)
+    graph.insert(2)
+    graph.insert(3)
+    try { graph.nodes = [] } catch {}
+    expect(graph.nodes.length).toBe(3)
+    let nodes = graph.nodes
+    nodes.push(4)
+    expect(graph.nodes.length).toBe(3)
+  })
+
+  it('Should establish links between nodes', ()=>{
+    let node1 = graph.insert(1)
+    let node2 = graph.insert(2)
+    graph.link(1, 2, 3)
+    expect(node1.links[0].node).toBe(node2)
+    expect(node1.links[0].cost).toBe(3)
+  })
+
+  it('Should error when linking a node to itself', ()=>{
+    graph.insert(1)
+    expect(()=>{
+      graph.link(1, 1, 2)
+    }).toThrow('Cannot link a node to itself')
+  })
+
+  it('Should error when linking values that do not exist', ()=>{
+    graph.insert(1)
+    expect(()=>{
+      graph.link(1, 2, 3)
+    }).toThrow('Attempted to link unincluded values')
+
+    graph = new Graph()
+    graph.insert('b')
+    expect(()=>{
+      graph.link('a', 'b', 3)
+    }).toThrow('Attempted to link unincluded values')
+  })
+
+  it('Should not create duplicate links', ()=>{
+    let node1 = graph.insert(1)
+    graph.insert(2)
+    graph.link(1, 2, 3)
+    graph.link(1, 2, 5)
+    expect(node1.links.length).toBe(1)
+  })
+
+  it('Should update cost for already-existing links', ()=>{
+    let node1 = graph.insert(1)
+    graph.insert(2)
+    graph.link(1, 2, 3)
+    expect(node1.links[0].cost).toBe(3)
+    graph.link(1, 2, 5)
+    expect(node1.links[0].cost).toBe(5)
+  })
+
+  describe('Find Minimum Traversal', ()=>{
+    beforeEach(()=>{
+      graph = new Graph()
+      for (let x = 0; x < 5; x++) {
+        graph.insert(x)
+      }
+      graph.linkAll(0)
+    })
+
+    it ('Should return an array', ()=>{
+      expect(graph.findMinimumTraversal(1).isArray()).toBe(true)
+    })
+
+    it ('Should begin with the given value', ()=>{
+      expect(graph.findMinimumTraversal(3)[0]).toBe(3)
+    })
+
+    it ('Should visit every node exactly once', ()=>{
+      let test = graph.findMinimumTraversal(0)
+      expect(test.length).toEqual(graph.size)
+      expect(Graph.values.every(value => test.includes(value))).toBe(true)
+    })
+
+    it ('Should return to start if second parameter is set to true', ()=>{
+      let test = graph.findMinimumTraversal(3)
+      expect(test[0]).toBe(3)
+      expect(test[test.length - 1]).toBe(3)
+    })
+
+    it ('Should find minimum cost traversal', ()=>{
+      graph.link(3, 0, 0)
+      graph.link(0, 2, 0)
+      graph.link(2, 4, 0)
+      graph.link(4, 1, 0)
+      graph.link(1, 3, 0)
+      expect(graph.findMinimumTraversal()).toEqual([3,0,2,4,1,3])
+    })
+  })
+
 })
